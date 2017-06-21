87c861dd8aef0b258b604911af05132fe9c075ec
emily <emily.liu@glowtouch.com>
2017-06-21 14:20:26 -0400

    fountain of youth for kiddos

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -30,19 +30,19 @@ INSERT INTO parents VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', 
 INSERT INTO parents VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', 'WI', '6085559435', 'david11');
 INSERT INTO parents VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', 'WI', '6085555487', 'carlos11');
 
-INSERT INTO kids VALUES (1, 'Alyssa', '2000-09-07', 2, 1, 'Claritin', 'None');
-INSERT INTO kids VALUES (2, 'Joe', '2002-08-06', 1, 2, 'None', 'Lipitor');
-INSERT INTO kids VALUES (3, 'Lauren', '2001-04-17', 2, 3, 'None', 'None');
-INSERT INTO kids VALUES (4, 'Nicole', '2000-03-07', 2, 3, 'Penicilin', 'None');
-INSERT INTO kids VALUES (5, 'Thomas', '2000-11-30', 1, 4, 'None', 'Plavix');
-INSERT INTO kids VALUES (6, 'Samantha', '2000-01-20', 2, 5, 'Latex', 'Advair Diskus');
-INSERT INTO kids VALUES (7, 'George', '1995-09-04', 1, 6, 'Insulin', 'None');
-INSERT INTO kids VALUES (8, 'Max', '1995-09-04', 1, 6, 'None', 'Singulair');
-INSERT INTO kids VALUES (9, 'Brendan', '1999-08-06', 1, 7, 'None', 'Actos');
-INSERT INTO kids VALUES (10, 'Elizabeth', '1997-02-24', 2, 8, 'None', 'None');
-INSERT INTO kids VALUES (11, 'Lucy', '2000-03-09', 2, 9, 'Iodine', 'None');
-INSERT INTO kids VALUES (12, 'Sunny', '2000-06-24', 2, 10, 'None', 'None');
-INSERT INTO kids VALUES (13, 'Conner', '2002-06-08', 1, 10, 'None', 'Epogen');
+INSERT INTO kids VALUES (1, 'Alyssa', '2010-09-07', 2, 1, 'Claritin', 'None');
+INSERT INTO kids VALUES (2, 'Joe', '2012-08-06', 1, 2, 'None', 'Lipitor');
+INSERT INTO kids VALUES (3, 'Lauren', '2011-04-17', 2, 3, 'None', 'None');
+INSERT INTO kids VALUES (4, 'Nicole', '2010-03-07', 2, 3, 'Penicilin', 'None');
+INSERT INTO kids VALUES (5, 'Thomas', '2010-11-30', 1, 4, 'None', 'Plavix');
+INSERT INTO kids VALUES (6, 'Samantha', '2010-01-20', 2, 5, 'Latex', 'Advair Diskus');
+INSERT INTO kids VALUES (7, 'George', '2015-09-04', 1, 6, 'Insulin', 'None');
+INSERT INTO kids VALUES (8, 'Max', '2015-09-04', 1, 6, 'None', 'Singulair');
+INSERT INTO kids VALUES (9, 'Brendan', '2019-08-06', 1, 7, 'None', 'Actos');
+INSERT INTO kids VALUES (10, 'Elizabeth', '2017-02-24', 2, 8, 'None', 'None');
+INSERT INTO kids VALUES (11, 'Lucy', '2010-03-09', 2, 9, 'Iodine', 'None');
+INSERT INTO kids VALUES (12, 'Sunny', '2010-06-24', 2, 10, 'None', 'None');
+INSERT INTO kids VALUES (13, 'Conner', '2012-06-08', 1, 10, 'None', 'Epogen');
 
 INSERT INTO visits VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits VALUES (2, 8, '2013-01-02', 'rabies shot');
